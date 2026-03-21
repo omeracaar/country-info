a197e3c67ced10f43a687b3bacd2e07f148179b0
Ömer Acar <omeracar34@icloud.com>
2026-03-22 01:02:30 +0300

    feat: added getCountryName method

--- src/main/java/com/omeracar/country_info/controller/CountryInfoController.java
+++ src/main/java/com/omeracar/country_info/controller/CountryInfoController.java
@@ -1,4 +1,24 @@
 package com.omeracar.country_info.controller;
 
+import com.omeracar.countryinfo.ws.CountryInfoService;
+import com.omeracar.countryinfo.ws.CountryInfoServiceSoapType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/country")
 public class CountryInfoController {
+
+    private CountryInfoServiceSoapType getSoap() {
+        CountryInfoService countryInfoService = new CountryInfoService();
+        return countryInfoService.getCountryInfoServiceSoap();
+    }
+
+    @GetMapping("/name/{countryName}")
+    public String getCountryName(@PathVariable String countryName) {
+        return getSoap().countryName(countryName);
+    }
+
 }
