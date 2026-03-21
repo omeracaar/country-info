7b4de74659ea98d83599252a9e4fc92cd1fa7670
Ömer Acar <omeracar34@icloud.com>
2026-03-21 23:19:15 +0300

    initial commit

---
+++ src/main/java/com/omeracar/country_info/CountryInfoApplication.java
@@ -0,0 +1,13 @@
+package com.omeracar.country_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omeracar/country_info/CountryInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeracar.country_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
