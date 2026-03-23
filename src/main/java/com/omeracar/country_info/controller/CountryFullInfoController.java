05d2e7006bc8f7e4518b904087c6ade51e205284
Ömer Acar <omeracar34@icloud.com>
2026-03-23 12:00:19 +0300

    final commit

--- src/main/java/com/omeracar/country_info/controller/CountryFullInfoController.java
+++ src/main/java/com/omeracar/country_info/controller/CountryFullInfoController.java
@@ -1,4 +1,25 @@
 package com.omeracar.country_info.controller;
 
+import com.omeracar.country_info.dto.CountryFullInfoResponse;
+import com.omeracar.country_info.service.CountryFullInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/country-full-info")
 public class CountryFullInfoController {
+
+    @Autowired
+    private CountryFullInfoService countryFullInfoService;
+
+    @GetMapping("/{countryCode}")
+    public CountryFullInfoResponse getFullCountryInfo(@PathVariable String countryCode) {
+        return countryFullInfoService.countryFullInfoResponse(countryCode);
+    }
+
+
+
 }
